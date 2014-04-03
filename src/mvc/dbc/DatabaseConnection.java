3f302d9505b92ddbece3be792834a3172b236064
Chengjiao Yang <cyang@wpi.edu>
2014-04-03 14:51:13 -0400

    use config file to connect database

--- src/mvc/dbc/DatabaseConnection.java
+++ src/mvc/dbc/DatabaseConnection.java
@@ -1,25 +1,64 @@
 //负责数据库的打开与关闭操作 
 package mvc.dbc;
 
+import java.io.FileInputStream;
 import java.sql.*;
+import java.util.Properties;
 
 public class DatabaseConnection {
+	private static String DBdriver = null;
+	private static String DBURL = null;
+	private static String DBUSER = null;
+	private static String DBPASS = null;
+	private static String configname = "config.properties";
+	private Connection conn = null;
 	//below is db4free online mysql database(Denmark) 
-	//private static final String DBURL = "jdbc:mysql://www.db4free.net:3306/cs509"; 
-	//private static final String DBUSER = "cs509"; 
-	//private static final String DBPASS ="509123"; 
+	/*db4free
+	private static final String DBURL = "jdbc:mysql://www.db4free.net:3306/cs509"; 
+	private static final String DBUSER = "cs509"; 
+	private static final String DBPASS ="509123"; */
+	/*localhost
 	private static final String DBdriver = "com.mysql.jdbc.Driver";
 	private static final String DBURL = "jdbc:mysql://127.0.0.1:3306/51test";
 	private static final String DBUSER = "root";
-	private static final String DBPASS = "1234";
-	/*private static final String DBdriver = "com.mysql.jdbc.Driver";
+	private static final String DBPASS = "1234";*/
+	/*zhouhao's
+	private static final String DBdriver = "com.mysql.jdbc.Driver";
 	private static final String DBURL = "jdbc:mysql://cs509.wpilife.org:3306/cs509_team2";
 	private static final String DBUSER = "team2";
 	private static final String DBPASS = "CS509TM2";*/
-	private Connection conn = null;
-
+	
+	public void GetDBFromXML() throws Exception{
+		//read file in WEB-INF
+		try {
+			String path = DatabaseConnection.class.getResource("/").getPath();  
+	        String websiteURL = (path.replace("/build/classes", "").replace("%20"," ").replace("classes/", "") + configname).replaceFirst("/", "");  
+	        setParameter(websiteURL); 
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private void setParameter(String path) throws Exception{
+		Properties properties = new Properties();
+		FileInputStream fileInputStream = null;
+		try {
+			fileInputStream = new FileInputStream(path);
+			properties.load(fileInputStream);
+			DBdriver = properties.getProperty("database.driverClassName");
+			DBURL = properties.getProperty("database.url");
+			DBUSER = properties.getProperty("database.username");
+			DBPASS = properties.getProperty("database.password");
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			fileInputStream.close();
+		}
+	}
+	
 	public DatabaseConnection() throws Exception { // 在构造方法中进行数据库连接
 		try {
+			GetDBFromXML();
 			Class.forName(DBdriver).newInstance();// 加载驱动程序
 			this.conn = DriverManager.getConnection(DBURL, DBUSER, DBPASS);// 连接数据库
 		} catch (Exception e) {
